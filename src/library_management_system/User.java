e8c8317cb7f1de98ed6b48c29f59457d163bdbe6
Neelesh <neeleshkumar.dev@gmail.com>
2026-09-01 19:49:31 +0530

    Create User class

--- src/library_management_system/User.java
+++ src/library_management_system/User.java
@@ -1,4 +1,51 @@
 package library_management_system;
 
+import java.util.List;
+
 public class User {
+    private int id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+
+    public void displayUserInfo() {
+        System.out.println(
+                "User ID: " + id +
+                        " | Name: " + name +
+                        " | Borrowed Books: " + borrowedBooks.size()
+        );
+
+    }
+
 }
